1f2fe402772cbecad2dcf5248172bcc0e7e9e0f9
mogor <m.ogorevc0@gmail.com>
2024-06-01 17:04:28 +0200

    street building system fully functioning

--- game/scene.py
+++ game/scene.py
@@ -1,8 +1,8 @@
 import random
 
 import config
-from entities.house import House
-from entities.street import Street
+from objects.house import House
+from objects.street import Street
 from utils.assets import house_img
 from utils.assets import street_img
 
@@ -26,24 +26,66 @@ class Scene:
                 if st.x == x and st.y == y:
                     valid = False
 
+            for house in houses:
+                if house.x == x and house.y == y:
+                    valid = False
+
             if valid:
                 street_counter -= 1
                 street = Street(screen, street_img, x, y)
                 for house in houses:
-                    if house.y == y and (house.x + 50 == x or house.x - 50 == x):
-                        _r = True
-                        house.add_street(street)
-                    elif house.x == x and (house.y + 50 == y or house.y - 50 == y):
-                        house.add_street(street)
+                    if house.y == y:
+                        if house.x + 50 == x:
+                            _r = True
+                            house.add_street(street)
+                            street.add_left(house)
+                        elif house.x - 50 == x:
+                            _r = True
+                            house.add_street(street)
+                            street.add_right(house)
+                    elif house.x == x:
+                        if house.y + 50 == y:
+                            house.add_street(street)
+                            street.add_top(house)
+                        elif house.y - 50 == y:
+                            house.add_street(street)
+                            street.add_bottom(house)
 
                 for st in streets:
-                    if st.y == y and (st.x + 50 == x or st.x - 50 == x):
-                        if st.r:
-                            _r = True
+                    if st.y == y:
+                        if st.x + 50 == x:
+                            if st.r:
+                                _r = True
+                            st.add_right(street)
+                            street.add_left(st)
+                        if st.x - 50 == x:
+                            if st.r:
+                                _r = True
+                            st.add_left(street)
+                            street.add_right(st)
+                    if st.x == x:
+                        if st.y + 50 == y:
+                            st.add_bottom(street)
+                            street.add_top(st)
+                        if st.y - 50 == y:
+                            st.add_top(street)
+                            street.add_bottom(st)
 
                 if _r:
-                    street.rotate()
+                    street.rotate(1)
 
+                street.check()
+
+                if street.top is not None and type(street.top) is Street:
+                    street.top.check()
+                if street.bottom is not None and type(street.bottom) is Street:
+                    street.bottom.check()
+                if street.left is not None and type(street.left) is Street:
+                    street.left.check()
+                if street.right is not None and type(street.right) is Street:
+                    street.right.check()
+
+                print(type(street.top), street.bottom, street.left, street.right)
                 streets.append(street)
 
     def update(self, game, screen, runtime, houses, streets):
@@ -64,6 +106,10 @@ class Scene:
 
                     for house in houses:
                         if house.x == x and house.y == y or house.x == x + 50 or house.x == x - 50 or house.y == y + 50 or house.y == y - 50:
+                            find_x_y = True
+
+                    for street in streets:
+                        if street.x == x and street.y == y:
                             find_x_y = True
 
                 houses.append(House(screen, house_img, x, y))
---
+++ objects/street.py
@@ -0,0 +1,84 @@
+import pygame
+
+from utils.assets import *
+
+
+class Street:
+    def __init__(self, screen, img, x, y):
+        self.screen = screen
+        self.img = img
+        self.offset_x = 12.5
+        self.offset_y = 0
+        self.x = x
+        self.y = y
+        self.r = False
+        self.top = None
+        self.bottom = None
+        self.left = None
+        self.right = None
+
+    def add_top(self, top):
+        self.top = top
+
+    def add_bottom(self, bottom):
+        self.bottom = bottom
+
+    def add_left(self, left):
+        self.left = left
+
+    def add_right(self, right):
+        self.right = right
+
+    def draw(self):
+        self.screen.blit(self.img, (self.offset_x + self.x, self.offset_y + self.y))
+
+    def rotate(self, n):
+        self.r = True
+        self.img = pygame.transform.rotate(self.img, 90 * n)
+        self.offset_x = 0
+        self.offset_y = 12.5
+
+    def check(self):
+        if self.top is not None and self.bottom is not None and self.left is not None and self.right is not None:
+            self.img = junction_img
+            self.offset_x = 0
+            self.offset_y = 0
+        elif self.top is not None and self.right is not None and self.bottom is not None:
+            self.img = t_junction_img
+            self.rotate(1)
+            self.offset_x = 12.5
+            self.offset_y = 0
+        elif self.right is not None and self.bottom is not None and self.left is not None:
+            self.img = t_junction_img
+            self.offset_x = 0
+            self.offset_y = 12.5
+        elif self.bottom is not None and self.left is not None and self.top is not None:
+            self.img = t_junction_img
+            self.rotate(3)
+            self.offset_x = 0
+            self.offset_y = 0
+        elif self.left is not None and self.top is not None and self.right is not None:
+            self.img = t_junction_img
+            self.rotate(2)
+            self.offset_x = 0
+            self.offset_y = 0
+        elif self.top is not None and self.right is not None:
+            self.img = corner_img
+            self.offset_x = 12.5
+            self.offset_y = 0
+
+        elif self.right is not None and self.bottom is not None:
+            self.img = corner_img
+            self.rotate(3)
+            self.offset_x = 12.5
+            self.offset_y = 12.5
+        elif self.bottom is not None and self.left is not None:
+            self.img = corner_img
+            self.rotate(2)
+            self.offset_x = 0
+            self.offset_y = 12.5
+        elif self.left is not None and self.top is not None:
+            self.img = corner_img
+            self.rotate(1)
+            self.offset_x = 0
+            self.offset_y = 0
